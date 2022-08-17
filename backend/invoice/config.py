2c2689bcf6c85899a66a59271b4b57b1e6e079de
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-08-17 13:25:29 +0700

    Implement skeleton for invoice service

---
+++ backend/invoice/config.py
@@ -0,0 +1,3 @@
+import os
+
+invoice_create_topic = os.getenv('INVOICE_CREATE_TOPIC_NAME', 'invoice-create-topic')
--- backend/invoice/main.py
+++ backend/invoice/main.py
@@ -1,15 +1,30 @@
-from typing import Union
-
+import uvicorn
+import asyncio
+import logging
 from fastapi import FastAPI
+from aredis_om import get_redis_connection
+from config import invoice_create_topic
+from redis.client import PubSub
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+log = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def startup_event():
+    client = get_redis_connection()
+    pubsub = client.pubsub()
+    await pubsub.subscribe(invoice_create_topic)
+    asyncio.create_task(subscribe_topic(pubsub))
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def subscribe_topic(pubsub: PubSub):
+    async for message in pubsub.listen():
+        log.info(f'Message: {message}')
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
