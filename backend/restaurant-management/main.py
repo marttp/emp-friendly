ceb8c1c1ba1d3162f129b41235a0484781589c3e
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-08-20 20:26:33 +0700

    Implement Restaurant Management service

--- backend/restaurant-management/main.py
+++ backend/restaurant-management/main.py
@@ -1,15 +1,33 @@
-from typing import Union
-
+import logging
+import uvicorn
 from fastapi import FastAPI
+from redis_om import Migrator
+from models import RestaurantModel
+from restaurants import restaurants, create, delete
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+log = logging.getLogger(__name__)
 
 app = FastAPI()
+Migrator().run()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def startup_event():
+    restaurant_list = [
+        RestaurantModel(id='a3cbc4f1-3636-4db9-bb42-36c49ba655b9', name='SHOP-1'),
+        RestaurantModel(id='d72d77fb-e96d-4d9a-964d-f2bf605c7e0b', name='SHOP-2'),
+        RestaurantModel(id='c820da4c-d8de-4229-848b-33e5f183a22c', name='SHOP-3'),
+        RestaurantModel(id='f084e35a-f745-4c86-b0d5-aae81bd632a9', name='SHOP-4'),
+        RestaurantModel(id='d31710fc-ba84-4ef9-ae2e-3c8e38e1c84c', name='SHOP-5')
+    ]
+    for r in restaurant_list:
+        await delete(r.id)
+        await create(r)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(restaurants, prefix='/restaurants', tags=['restaurants'])
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
---
+++ backend/restaurant-management/restaurants.py
@@ -0,0 +1,36 @@
+from typing import List
+from fastapi import APIRouter, HTTPException
+from models import Restaurant, RestaurantModel
+from aredis_om import NotFoundError
+import logging
+
+log = logging.getLogger(__name__)
+restaurants = APIRouter()
+
+
+@restaurants.post('/', response_model=Restaurant, status_code=201)
+async def create(payload: RestaurantModel):
+    log.info("Create Restaurant")
+    restaurant = Restaurant(
+        restaurant_id=payload.id,
+        name=payload.name
+    )
+    restaurant.save()
+    return restaurant
+
+
+@restaurants.get('/', response_model=List[Restaurant])
+async def read() -> List[Restaurant]:
+    log.info("Read All Restaurant")
+    return Restaurant.find().all()
+
+
+@restaurants.delete('/{id}', response_model=None, status_code=204)
+async def delete(id: str):
+    log.info("Delete Restaurant")
+    try:
+        restaurant_list = Restaurant.find(Restaurant.restaurant_id == id).all()
+        for r in restaurant_list:
+            r.delete(r.pk)
+    except NotFoundError:
+        raise HTTPException(status_code=404, detail="Restaurant not found")
