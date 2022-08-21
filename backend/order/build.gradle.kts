19082c798411017775628087ef3705235e4eaf15
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-08-21 12:46:04 +0700

    Add Jib for order service

--- backend/order/build.gradle.kts
+++ backend/order/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+	id("com.google.cloud.tools.jib") version "3.2.1"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -24,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.redis.om:redis-om-spring:0.5.2-SNAPSHOT")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
