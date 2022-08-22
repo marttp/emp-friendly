4dd81cba90ed32ed286d70277b04195a86bec3a7
Thanaphoom Babparn <tp.babparn@gmail.com>
2022-08-22 16:35:29 +0700

    Update RequestMapping

--- backend/management/src/main/java/dev/tpcoder/empfriendly/management/point/PointController.java
+++ backend/management/src/main/java/dev/tpcoder/empfriendly/management/point/PointController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import reactor.core.publisher.Mono;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 @Controller
 @ResponseBody
 @RequiredArgsConstructor
+@RequestMapping("/points")
 public class PointController {
   private final PointService pointService;
 
